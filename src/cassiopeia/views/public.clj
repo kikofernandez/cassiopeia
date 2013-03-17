6c68bf70426bbd43fac0cf3d18e112c2286527be
Kiko Fernandez <kiko@portato.se>
2013-03-17 19:55:38 +0100

    changed the index page

--- src/cassiopeia/views/public.clj
+++ src/cassiopeia/views/public.clj
@@ -9,32 +9,48 @@
                 [:h1 "We enlighten your business"]]
 
                ; slider
-               [:ul {:data-orbit ""}
-                [:li 
-                 (element/image "/img/html5-badge.png")
-                 [:div {:class "orbit-caption"} "Welcome to HTML5, welcome to the future"]]
-                [:li
+               [:div {:class "row"}
+                [:div {:class "large-7 columns"}
                  (element/image "/img/sencha-touch-2-devices-400.jpg")
-                 [:div {:class "orbit-caption"} 
-                  "Build with the latest technology. Build one, deploy anywhere"]]
-                [:li
-                 (element/image "/img/sencha-tools-250.png")
-                 [:div {:class "orbit-caption"} "Proud of using Sencha Framework"]]]
-
+                ; [:ul {:data-orbit ""}
+                ;  [:li 
+                ;   (element/image {:height "200px"} "/img/Devices.png")
+                ;   [:div {:class "orbit-caption"} "Run everywhere"]]
+                ;  [:li
+                ;   (element/image {:height "200px"} "/img/sencha-touch-2-devices-400.jpg")
+                ;   [:div {:class "orbit-caption"} 
+                ;    "Build with the latest technology. Build one, deploy anywhere"]]
+                ;  [:li
+                ;   (element/image {:height "200px"} "/img/sencha-tools-250.png")
+                ;   [:div {:class "orbit-caption"} "Proud of using Sencha Framework"]]]
+                 ]
+                
+                [:div {:class "panel radius large-5 columns"} 
+                 [:p "Hemos creado la aplicación perfecta para aquellos negocios que
+                  necesitan realizar inventario de forma rápida y sencilla"]]
+                ]
+               
+               ;[:div {:class "row"}
+               ; [:div {:class "panel large-12"}
+               ;  [:p "Hemos creado la aplicación perfecta para aquellos negocios que
+               ;   necesitan realizar inventario de forma rápida y sencilla"]]] 
+               
                ;Bottom Feature panels
                [:div {:class "row"}
                 [:div {:class "large-4 columns"}
                  [:div {:class "panel"}
                   "Welcome to HTML5, welcome to the future"
-                 (element/image "/img/html5-badge.png")]]
+                 (element/image {:height "100px"} "/img/html5-badge.png")]]
+                
                 [:div {:class "large-4 columns"} 
                  [:div {:class "panel"}
                    "Build with the latest technology. Build one, deploy anywhere"
-                   (element/image "/img/sencha-touch-2-devices-400.jpg")]]
+                   (element/image {:height "100px"} "/img/sencha-touch-2-devices-400.jpg")]]
+                
                 [:div {:class "large-4 columns"} 
                  [:div {:class "panel"}
                    "Proud of using Sencha Framework"
-                   (element/image "/img/sencha-tools-250.png")]]]))
+                   (element/image {:height "100px"} "/img/sencha-tools-250.png")]]]))
 
 (def feature-showcase
   (hpage/html5 [:div {:class "row"}
@@ -48,16 +64,12 @@
                     [:h3 "Run everywhere"]]]
                   [:div {:class "row"}
                    [:div {:class "large-7 columns"}
-;                    [:h2 "Run everywhere"]
                     [:p "Cassiopeia está diseñada para funcionar bajo cualquier plataforma ofreciendo
                    el máximo rendimiento posible en cada una de ellas. Actualmente soportamos
                    Android y iOs."]]
                    [:div {:class "large-5 columns"}
                     (element/image "/img/Devices.png")]]
                   
-;                  [:div {:class "row"}
-;                   [:div {:class "large-7 large-offset-5 columns"}
-;                    [:h2 "We love the cloud"]]]
                   ;; Second feature
                   [:div {:class "row"}
                    [:div {:class "large-5 columns"}
@@ -78,20 +90,19 @@
                       automática cuando tenga conexión. La sincronización es ahora un
                       juego de niños."]]
                    [:div {:class "large-5 columns"}
-                    (element/image "/img/online_to_offline_connection.png")]]]
-                 ;[:div {:class "large-7 large-offset-5"}
-                 ; [:h2 "We love the Cloud"]
-                 ; [:p "La aplicación funciona en la nube, de manera que siempre tendrá
-                 ;  a su disposición los datos sin necesidad de emplear complicadas instalaciones.
-                 ;  Los datos son privados y le pertenecen a usted, nosotros sólo nos 
-                 ;  encargamos de custodiarlos."]]
-                 
-                 ;[:div {:class "large-7"}
-                 ; [:h2 "Offline store"]
-                 ; [:p "Utilice la aplicación estando offline y sincronice de forma
-                 ;  automática cuando tenga conexión. La sincronización es ahora un
-                 ;  juego de niños."]]
-                 ]]))
+                    (element/image "/img/online_to_offline_connection.png")]]
+                  
+                  ;; Fourth feature
+                  [:div {:class "row"}
+                   [:div {:class "large-5 columns"}
+                    ;(element/image "img/Cloud.png")]
+                    (element/image "img/ai.jpg")]
+                   [:div {:class "large-7 columns"}
+                    [:h3 "Let us think for you"]
+                    [:p "Cassiopeia contiene un sistema experto que le mostrará datos
+                     estadísticos presentes y, usando las más avanzadas técnicas en
+                     Inteligencia Artificial, predicciones basadas en los datos recogidos."]]]
+                  ]]]))
 
 (def waiting-list-view
   (hpage/html5 [:div {:class "row"}
