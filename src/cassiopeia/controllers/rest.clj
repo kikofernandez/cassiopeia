462bf60ca04496f52f99b7b09b4418d13a391928
Kiko Fernandez <kikofernandezreyes@gmail.com>
2013-05-11 18:48:12 +0200

    almost ready to start creating questions

--- src/cassiopeia/controllers/rest.clj
+++ src/cassiopeia/controllers/rest.clj
@@ -3,7 +3,21 @@
   (:require [compojure.core :as compoj]
             [compojure.route :as route]
             [ring.util.response :as resp]
-            [sandbar.stateful-session :as session]))
+            [sandbar.stateful-session :as session]
+            [cassiopeia.models.private :as model]))
+
+(defn- save-questionnaire
+  [{:keys [user questionnaire] :as data}]
+  (doseq [i (range (count questionnaire))]
+    (println (model/question i data))
+    )
+;  (map (model/question) )
+  ;(model/save-question {:first_name (:first_name user)
+  ;                      :last_name (:last_name user)
+  ;                      :title ("xxx")
+  ;                      })
+  )
+
 
 (defroutes routes
   (compoj/GET "/" [] ("hola"))
@@ -13,6 +27,10 @@
                ; [{id 1, title "Jaja"} {id 2, title "Jojo"} ...]
                (println ((first body) "title"))
                (println (session/session-get :user))
+               ;(println {:user (session/session-get :user nil),
+               ;                     :questionnaire body})
+               (save-questionnaire {:user (session/session-get :user nil),
+                                    :questionnaire body})
 
                ; using Cheshire to map the received json into a dict {:id 1}
                ;(println ((json/parse-string (json/generate-string (first body)) true) :title))
--- src/cassiopeia/models/private.clj
+++ src/cassiopeia/models/private.clj
@@ -13,12 +13,12 @@
    :small-account small-account
    :premium-account premium-account})
 
-(defn user-model 
+(defn user-model
   "Creates a user model to be populated with information"
   [{:as attributes}]
-   (merge {:name "" 
-           :first_name "Janina" 
-           :last_name "" 
+   (merge {:name ""
+           :first_name "Janina"
+           :last_name ""
            :account (accounts :free-account)}
           attributes))
 
@@ -27,28 +27,45 @@
   [{:as attributes}]
    (merge {:name "Documents"
            :description "Default category"
-           :tags ["documents" "default"]} 
+           :tags ["documents" "default"]}
           attributes))
+
+(defn- answers-mapping
+  [answers]
+  (map (fn [data] {:title (data "title")}) answers))
 
 (defn question
   "Question default model"
-  []
-  {:first_name ""
-   :last_name ""
-   :title ""
-   :qid ""
-   :answers {}})
+  ([]
+   {:first_name ""
+    :last_name ""
+    :title ""
+    :qid ""
+    :answers {}})
+  ([index {:keys [user questionnaire]}]
+   {:first_name (:first_name user)
+    :last_name (:last_name user)
+    :title ((questionnaire index) "title")
+    :answers (-> ((questionnaire index) "answers")
+                 (answers-mapping))}))
+;    :answers ((questionnaire index) "answers")}))
 
+(defn save-question
+  [data]
+  (mc/insert "question" data))
+
+
+; not used?
 (defn item
   "Defines a item, which is a question and a list of valid answers"
   [{:as attributes}]
-  (merge {:name "Enter a name" 
+  (merge {:name "Enter a name"
           :description "Enter a description"
           :answers []}
          attributes))
 
 (defn all
-  "Display all valid questions. 
+  "Display all valid questions.
   A valid question is a question that has a title."
   [user]
   (filter :title (mc/find-maps "question" {:first_name (:first_name user)})))
